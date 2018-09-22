290178ce1d4d326aa3b897a4b7622904eb6a212d
elizg <elizabethgrauvogel@gmail.com>
2018-09-21 18:53:03 -0700

    Completed app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -47,14 +47,27 @@ function Square(props) {
           };
       }
 
-      handleClick(i) {
-        const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState( { 
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-         } );
-      }
+      // before if statement is added to halt click if a player has alread won
+      // handleClick(i) {
+      //   const squares = this.state.squares.slice();
+      //   squares[i] = this.state.xIsNext ? 'X' : 'O';
+      //   this.setState( { 
+      //       squares: squares,
+      //       xIsNext: !this.state.xIsNext,
+      //    } );
+      // }
+
+    handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState( {
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    } );
+  }  
 
     renderSquare(i) {
       // return <Square value = {i} />;
@@ -69,10 +82,21 @@ function Square(props) {
             />
         );     
     }
-  
-    // will display which player is next
+    
+    // Specifies winner
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
+
+    // will display which player is next
+    // render() {
+    //   const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
@@ -104,21 +128,36 @@ function Square(props) {
           <div className="game-board">
             <Board />
           </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
         </div>
       );
     }
   }
-  
-  // Declare winner by adding helper fx
-  // return tomorrow to add this!!
-
 
   ReactDOM.render(
     <Game />,
     document.getElementById('root')
   );
   
+// Declare winner by adding helper fx
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  // go back to review entire Board class and rewrite!
+  // verify game class
